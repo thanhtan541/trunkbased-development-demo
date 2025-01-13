8d867275fbacbd8a35e7ec2e96285af4636fb5d7
tan541 <tan@uney.com>
2025-01-13 12:39:17 +0700

    feat: update scripts

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -3,3 +3,6 @@
 set -x
 # Immediately exit on first fail command and print the first exit status of the first failing command in the pipeline.
 set -eo pipefail
+
+source ./scripts/init_kustomize.sh
+source ./scripts/init_k8s_mac_local.sh
